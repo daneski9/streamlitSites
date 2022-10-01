39bce66254ab22a54d6946670d738045f531a48b
Dane Coleman <danecoleman@csus.edu>
2022-09-30 20:41:00 -0700

    trying with my database

--- 174Test/mysqlStuff.py
+++ 174Test/mysqlStuff.py
@@ -20,7 +20,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from petsTest;")
+rows = run_query("SELECT * from mytable;")
 
 # Print results.
 for row in rows:
