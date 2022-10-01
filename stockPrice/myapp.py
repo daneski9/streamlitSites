151f75ee8211c4a2d42b11d7b022490e3aabfa26
Dane Coleman <danecoleman@csus.edu>
2022-09-30 18:01:16 -0700

    ba

--- stockPrice/myapp.py
+++ stockPrice/myapp.py
@@ -5,7 +5,7 @@ import pandas as pd
 tickerSymbol = 'BA'
 st.write("""
 # Simple Stock Price App
-Shown are the stock closing price and volume of `tickerSymbol`!
+Shown are the stock closing price and volume of BA!
 """)
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
